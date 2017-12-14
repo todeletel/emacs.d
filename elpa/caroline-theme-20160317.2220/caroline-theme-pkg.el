34e007d7297fc819a94b30d85320baccf11c46f4
Sengege <trace_tristan@126.com>
2017-12-14 15:14:56 +0800

    rm elpy

--- elpa/caroline-theme-20160317.2220/caroline-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "caroline-theme" "20160317.2220" "A trip down to New Orleans..." '((emacs "24")) :commit "222fd483db304509f9e422dc82883d808e023ceb" :url "https://github.com/xjackk/carolines-theme")
