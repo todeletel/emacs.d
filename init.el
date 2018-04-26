5d2371987bad0e55b0ae171381b838991d363d23
Sengege <trace_tristan@126.com>
2018-04-26 17:08:58 +0800

    add init info for first use

--- init.el
+++ init.el
@@ -4,11 +4,10 @@
 ;; You may delete these explanatory comments.  Install auto by install
 ;; package.
 
-
+;; this line will failed by unkowns error. manual run eval-buffer for this file
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
-
 (require 'init-packages)
 (require 'init-keybindings)
 (require 'init-ui)
