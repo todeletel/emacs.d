2a7c520d352e75ad28b7569fb2f1804e7a7a3f11
int9tristan <trace_tristan@126.com>
2018-12-19 11:31:57 +0800

    change setting

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1,76 +1,77 @@
-
-(when (>= emacs-major-version 24)
-;; (add-to-list 'package-archives
-;;	     '("melpa" . "http://melpa.org/packages/") t)
+;;hen (>= emacs-major-version 24)
+ ;;add-to-list 'package-archives
+;;	       '("melpa" . "http://melpa.org/packages/") t)
+;;equire 'cl)
 
 ;;add whateve packages you want here
-(defvar tristan/packages '(
-			   company
-			   monokai-theme
-			   hungry-delete
-			   swiper
-			   counsel
-			   smartparens
+
+;;efvar tristan/packages '(
+;;			   company
+;;			   monokai-theme
+;;			   hungry-delete
+;;			   swiper
+;; 			   counsel
+;;			   smartparens
 			   ;;js2-mode
-			   ;;nodejs-repl
-			   popwin
+  			   ;;nodejs-repl
+;;			   popwin
 			   ;;treemacs
-			   shell-pop
-			   spacemacs-theme
-			   spaceline
-			   multi-term
-			   elpy
-			   magit
-			   helm-ag
-			   nyan-mode
+;;			   shell-pop
+;;			   spacemacs-theme
+;;			   spaceline
+;;			   multi-term
+;;			   elpy
+;;			   magit
+;;			   helm-ag
+;; 			   nyan-mode
 			   ;;org-trello
-			   ) "Default packages")
+;; 			   ) "Default packages")
 
 
-(defun tristan/packages-installed-p ()
-  (loop for pkg in tristan/packages
-	when (not (package-installed-p pkg))
-	do (return nil)
-	finally (return t)
-	))
+;;efun tristan/packages-installed-p ()
+;;(loop for pkg in tristan/packages
+;;	when (not (package-installed-p pkg))
+;;	do (return nil)
+;; 	finally (return t)
+;;	))
 
 
-(unless (tristan/packages-installed-p)
-  (message "%s" "Fucking Refreshing package database....")
-  (package-refresh-contents)
-  (dolist (pkg tristan/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
+;;(unless (tristan/packages-installed-p)
+ ;; (message "%s" "Fucking Refreshing package database....")
+ ;; (package-refresh-contents)
+ ;; (dolist (pkg tristan/packages)
+  ;;  (when (not (package-installed-p pkg))
+   ;; (package-install pkg))))
 
 ;;mode-auto-install end 
 
 
 ;;smartparens
-(require 'smartparens-config)
-(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+;;(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
 ;;(smartparens-global-mode t)
 
 ;; 一次性删除多个空格
-(require 'hungry-delete)
-(global-hungry-delete-mode)
+;;(require 'hungry-delete)
+;;(global-hungry-delete-mode)
 
 
 ;;popwin 光标自动跳转新窗口
-(require 'popwin)
-(popwin-mode t)
+;;(require 'popwin)
+;;(popwin-mode t)
 
 
 ;;hacking search
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
+;;(ivy-mode 1)
+;;(setq ivy-use-virtual-buffers t)
 ;;company
-(global-company-mode t)
+;;(global-company-mode t)
 
 ;;config js2-mode for js files
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
+;;(setq auto-mode-alist
+;;      (append
+;;       '(("\\.js\\'" . js2-mode))
+;;       auto-mode-alist))
 
 (provide 'init-packages)
 
